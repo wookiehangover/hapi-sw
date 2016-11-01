30ee749ddd6e03de94e5467f06faef3cf1b0fade
Sam Breed <sam.breed@creditkarma.com>
2016-10-31 18:46:30 -0700

    adding an example app and expanding the readme

---
+++ examples/index.js
@@ -0,0 +1,69 @@
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+const path = require('path')
+
+console.log(__dirname)
+
+server.connection({ port: 9000 })
+
+server.register([
+  require('inert'),
+  {
+    register: require('../'),
+    options: {
+      verbose: true,
+      runtimeCaching: [
+            {
+              urlPattern: /https:\/\/unsplash.it\//,
+              handler: 'fastest',
+              options: {
+                debug: true
+              }
+            },
+            {
+                urlPattern: /https:\/\/unpkg.com\//,
+                handler: 'cacheFirst',
+                options: {
+                  debug: true
+                }
+            }
+        ]
+    }
+  }
+], (err) => {
+  if (err) {
+    throw err
+  }
+
+  server.route([
+    {
+      path: '/',
+      method: 'GET',
+      config: {
+        plugins: {
+          sw: {
+            dynamicUrlToDependencies: [
+              'index.html',
+            ]
+          }
+        }
+      },
+      handler: {
+        file: 'index.html'
+      }
+    },
+    {
+      path: '/{param*}',
+      method: 'GET',
+      handler: {
+          directory: {
+              path: path.resolve(__dirname)
+          }
+      }
+    }
+  ])
+
+  server.start((err) => {
+    console.log(`Server started on ${server.info.uri}`)
+  })
+})
